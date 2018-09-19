13815f2ecd865e9982a54b00723a330efcc52192
KevinBrother <1301239018@qq.com>
2018-09-19 17:43:48 +0800

    请求

--- proj/pages/index/index.js
+++ proj/pages/index/index.js
@@ -1,54 +1,154 @@
-//index.js
-//获取应用实例
-const app = getApp()
-
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    play: true,
+    pause: false,
+    point:"00:25:00"
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log(this.data)
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  },
+
+  go: function() {
+    console.log("this.data.play is ----- " + this.data.play);
+    if(this.data.play) {
+      this.start();
+      this.startInterval();
+    }else{
+      this.stop();
+      this.endInterval();
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+
+  start: function () {
+    console.log("------start-------");
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      play: false,
+      pause: true
     })
+   
+  },
+
+  stop: function () {
+    console.log("------stop-------");
+    this.setData({
+      play: true,
+      pause: false
+    })
+  },
+
+  restart: function() {
+    this.stop();
+    this.endInterval();
+    this.endVibrate();
+    this.setData({
+      point: "00:25:00"
+    })
+  },
+
+  // 开始计时器
+  startInterval: function() {
+    var that = this;
+    that.data.setInter = setInterval(function() {
+      var point = that.data.point;
+      if (point == "00:00:00") {
+        that.stop();
+        that.endInterval();
+        that.startVibrate();
+        return;
+      }
+
+      var arr = point.split(":");
+      var sum = parseInt(arr[1] * 60) + parseInt(arr[2]);
+      sum -= 1;
+
+      var rest1 = parseInt(sum / 60);
+      arr[1] = rest1 < 10 ? "0" + rest1 : rest1;
+
+      var rest2 = parseInt(sum % 60);
+      arr[2] = rest2 < 10 ? "0" + rest2 : rest2;
+
+      point = arr.join(":");
+
+      that.setData({
+        point: point
+      }) 
+    }, 1000)
+  },
+
+  // 关闭计时器
+  endInterval: function() {
+    var that = this;
+    clearInterval(that.data.setInter);
+  },
+
+  // 开始震动
+  startVibrate: function() {
+    var that = this;
+    that.data.vibrateInterval = setInterval(function() {
+      wx.vibrateLong()
+    }, 3000);
+  },
+
+  // 关闭震动
+  endVibrate: function() {
+    var that = this;
+    clearInterval(that.data.vibrateInterval);
   }
 })
